0243c36445e153f0c8a1ce72f4418613bcea7e5f
victoire67 <ansimavicky@gmail.com>
2026-07-15 19:19:21 +0200

    chore: added framer motion

--- src/components/MenuExploreCard.tsx
+++ src/components/MenuExploreCard.tsx
@@ -1,21 +1,28 @@
-
+import {motion} from "framer-motion"
 type MenuExplorerCardTp = {
     img: string,
     type: string,
     description: string
 }
 export default function MenuExploreCard({ img, type, description }: MenuExplorerCardTp) {
-    return <div className="sm:w-120 w-full  relative h-80 rounded-sm overflow-hidden bg-linear-to-t from-black to-transparent  shadow-lg cursor-pointer group">
-        <img src={img} alt="food" className=" absolute -z-1 group-hover:scale-120 transform transition-all durantion-500" />
-        <div className="h-full flex items-end p-8 text-[32px]">
-            <div className="flex items-end place-content-between  w-full">
-                <section>
-                    <h1>{type}</h1>
-                    <p className="text-primary text-[16px]">{description}</p>
-                </section>
-                <p className="text-primary text-[32px]">&rarr;</p>
+    return <motion.div
+        initial={{ opacity: 0, y: 40 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.3 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+    >
+        <div className="sm:w-120 w-full  relative h-80 rounded-sm overflow-hidden bg-linear-to-t from-black to-transparent  shadow-lg cursor-pointer group">
+            <img src={img} alt="food" className=" absolute -z-1 group-hover:scale-120 transform transition-all durantion-500" />
+            <div className="h-full flex items-end p-8 text-[32px]">
+                <div className="flex items-end place-content-between  w-full">
+                    <section>
+                        <h1>{type}</h1>
+                        <p className="text-primary text-[16px]">{description}</p>
+                    </section>
+                    <p className="text-primary text-[32px]">&rarr;</p>
+                </div>
             </div>
         </div>
-    </div>
+    </motion.div>
 
 }
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,23 +2,39 @@ import Header from "../components/Header"
 import MenuExploreCard from "../components/MenuExploreCard"
 import food from "../assets/breakfast.jpg"
 import Footer from "../components/Footer"
+import { motion } from "framer-motion"
 export default function LandingPage() {
     return <div >
 
         <div className="w-full h-screen bg bg-center bg-cover">
+
             <div className="pt-24 text-center sm:w-1/2 mx-auto">
-                <h1 className="text-center sm:text-5xl text-4xl font-bold pb-8">MOUNTAIN VIEW HOTEL</h1>
-                <h1>Enjoy delicious local and international cuisine, refreshing cocktails, and a
-                    relaxing atmosphere at Mountain View Restaurant, Bar & Pool Bar. Whether
-                    you're dining with family, meeting friends, or unwinding by the pool, we offer the
-                    perfect setting for every occasion.</h1>
+                <motion.div
+                    initial={{ opacity: 0, y: 40 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true, amount: 0.3 }}
+                    transition={{ duration: 0.6, ease: "easeOut" }}
+                >
+                    <h1 className="text-center sm:text-5xl text-4xl font-bold pb-8">MOUNTAIN VIEW HOTEL</h1>
+                    <h1>Enjoy delicious local and international cuisine, refreshing cocktails, and a
+                        relaxing atmosphere at Mountain View Restaurant, Bar & Pool Bar. Whether
+                        you're dining with family, meeting friends, or unwinding by the pool, we offer the
+                        perfect setting for every occasion.</h1>
 
+                </motion.div>
 
             </div>
-            <div className="mt-16 mx-auto flex items-center place-content-center gap-8">
-                <button className="text- px-8 py-2 bg-[#FFB82B] text-black border-amber-200 border hover:text-[#FFB82B] hover:bg-transparent cursor-pointer transform transition-colors">MENU</button>
-                <button className="text-[#FFB82B] px-8 py-2 border-amber-200 border hover:text-black hover:bg-[#FFB82B] cursor-pointer transform transition-colors">CONTACT</button>
-            </div>
+            <motion.div
+                initial={{ opacity: 0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true, amount: 0.3 }}
+                transition={{ duration: 0.6, ease: "easeOut" }}
+            >
+                <div className="mt-16 mx-auto flex items-center place-content-center gap-8">
+                    <button className="text- px-8 py-2 bg-[#FFB82B] text-black border-amber-200 border hover:text-[#FFB82B] hover:bg-transparent cursor-pointer transform transition-colors">MENU</button>
+                    <button className="text-[#FFB82B] px-8 py-2 border-amber-200 border hover:text-black hover:bg-[#FFB82B] cursor-pointer transform transition-colors">CONTACT</button>
+                </div>
+            </motion.div>
         </div>
 
         <div>
@@ -28,7 +44,7 @@ export default function LandingPage() {
             <div className="sm:flex items-center gap-8 place-content-center">
                 <MenuExploreCard img={food} type="Food" description="Explore the best food in Rwanda" />
                 <MenuExploreCard img={food} type="Drinks" description="Explore the best food in Rwanda" />
-            </div>  
+            </div>
         </div>
     </div>
 
