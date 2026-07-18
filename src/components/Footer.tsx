7ead24b40dcd68394a4903981e29aef5f07625d7
victoire67 <ansimavicky@gmail.com>
2026-07-18 18:17:09 +0200

    fix: fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
-    <footer className="mt-12 border-t border-neutral-800 pt-8 pb-8 text-sm" id="footer">
-      <div className="max-w-4xl mx-auto px-4 text-neutral-400 w-full">
+    <footer className="border-t border-neutral-800 pt-4  text-sm w-full " id="footer">
+      <div className="max-w-4xl px-4 text-neutral-400 w-full">
         <div className="flex flex-col md:flex-row md:justify-between gap-6">
           <div>
             <div className="font-semibold">Mountain View Hotel</div>
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,7 @@
 import MenuExploreCard from "../components/MenuExploreCard"
 import food from "../assets/breakfast.jpg"
 import { motion } from "framer-motion"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 export default function LandingPage() {
     return <div >
 
@@ -31,8 +31,7 @@ export default function LandingPage() {
             >
                 <div className="mt-16 mx-auto flex items-center place-content-center gap-8">
                     <a href="#explore"><button className="text- px-8 py-2 bg-[#FFB82B] text-black border-amber-200 border hover:text-[#FFB82B] hover:bg-transparent cursor-pointer transform transition-colors">MENU</button></a>
-                    <button className="text-[#FFB82B] px-8 py-2 border-amber-200 border hover:text-black hover:bg-[#FFB82B] cursor-pointer transform transition-colors">CONTACT</button>
-                </div>
+                    <a href="#footer"> <button className="text-[#FFB82B] px-8 py-2 border-amber-200 border hover:text-black hover:bg-[#FFB82B] cursor-pointer transform transition-colors">CONTACT</button>  </a> </div>
             </motion.div>
         </div>
 
@@ -41,7 +40,7 @@ export default function LandingPage() {
                 Explore our Menu
             </h1>
             <div className="sm:flex items-center gap-8 place-content-center">
-               <Link to="/food"> <MenuExploreCard img={food} type="Food" description="Explore the best food in Rwanda" /></Link>
+                <Link to="/food"> <MenuExploreCard img={food} type="Food" description="Explore the best food in Rwanda" /></Link>
                 <MenuExploreCard img={food} type="Drinks" description="Explore the best food in Rwanda" />
             </div>
         </div>
