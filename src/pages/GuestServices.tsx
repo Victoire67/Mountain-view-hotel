9e9f7d73265957ba6febddf39896024b43c54641
victoire67 <ansimavicky@gmail.com>
2026-07-20 13:54:36 +0200

    fix: fixed type for swimming pool

--- src/pages/GuestServices.tsx
+++ src/pages/GuestServices.tsx
@@ -3,7 +3,7 @@ import pool from "../assets/guest-services/swimming pool.jpg"
 import appartment from "../assets/guest-services/appartment.jpg"
 export default function GuestServices(){
     return  <div className="sm:flex mt-16 sm:mt-24 sm:place-content-center gap-8    ">
-        <MenuExplorerCard img={pool} type="Wimming pool" description="Enjoy a swimming pool"/>
+        <MenuExplorerCard img={pool} type="Swimming pool" description="Enjoy a swimming pool"/>
         <MenuExplorerCard img={appartment} type = "Appartment" description="Affortable appartment for rent"/>
     </div>
 }
