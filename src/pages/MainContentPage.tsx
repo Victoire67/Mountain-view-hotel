1e303c405fce9f4d5b56edfd88bbb719c67fc19a
victoire67 <ansimavicky@gmail.com>
2026-08-18 14:24:07 +0200

    fixed background on main content

--- src/pages/MainContentPage.tsx
+++ src/pages/MainContentPage.tsx
@@ -122,6 +122,7 @@ export default function MainContentPage({ type }: MainContentPageProps) {
             <div
                 className="bg-black/70 sm:pt-[20%] pt-[50%] animation-appear text-center h-screen/2 pb-4 bg-cover bg-center bg-blend-multiply bg-linear-to-t from-black/90 to-transparent"
                 style={{ backgroundImage: `url(../../public/${type}s/${[...currentCategory.categoryName].filter(x => x != " ").join("")}.jpg)` }}
+
             >
                 <AnimatePresence mode="wait">
                     <motion.div
